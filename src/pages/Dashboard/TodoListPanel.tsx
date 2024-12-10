bb230a15418974071090ee7468ec0c38429ddbb6
Piotr Pietruszka <p.pietruszka@MacBook-Pro.local>
2024-12-10 16:51:19 +0100

    feat: add error message

--- src/pages/Dashboard/TodoListPanel.tsx
+++ src/pages/Dashboard/TodoListPanel.tsx
@@ -10,22 +10,25 @@ export function TodoListPanel() {
 	const todos = useTodos();
 
 	return (
-		<div className="task-list-panel">
-			<TaskList>
-				{todos.map((todo) => (
-					<TaskList.Item
-						key={todo.id}
-						item={todo}
-						onClick={() => markComplete(todo.id)}
-						onRemove={() => deleteTodo(todo.id)}
-					/>
-				))}
-			</TaskList>
-			<TaskInputPanel
-				onAdd={(title) => {
-					addTodo({ id: todos.length + 1, title, completed: false });
-				}}
-			/>
+		<div>
+			{todos.error && <div className="error-message">{todos.error}</div>}
+			<div className="task-list-panel">
+				<TaskList>
+					{todos.data.map((todo) => (
+						<TaskList.Item
+							key={todo.id}
+							item={todo}
+							onClick={() => markComplete(todo.id)}
+							onRemove={() => deleteTodo(todo.id)}
+						/>
+					))}
+				</TaskList>
+				<TaskInputPanel
+					onAdd={(title) => {
+						addTodo({ id: todos.data.length + 1, title, completed: false });
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
